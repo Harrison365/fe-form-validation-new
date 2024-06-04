62e09c11bb4d9e7ea95e2e83f497811d2f37d819
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-06-04 10:42:09 +0100

    added form validation

---
+++ horoscope.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("horoscopeForm");
+
+form.addEventListener("submit", (event) => {
+  const firstName = document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const starSign = document.getElementById("starSign");
+  if (
+    firstName.value.trim() === "" ||
+    lastName.value.trim() === "" ||
+    starSign.value.trim() === ""
+  ) {
+    event.preventDefault(); //this needs to be first for some reason.
+    console.log("hi");
+    firstName.classList.add("error");
+    lastName.classList.add("error");
+    starSign.classList.add("error");
+  } else {
+    firstName.classList.remove("error");
+    lastName.classList.remove("error");
+    starSign.classList.remove("error");
+  }
+});
